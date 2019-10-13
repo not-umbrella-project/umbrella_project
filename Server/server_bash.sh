e3dc5fba2f143fbd318bbedcbcfc1502f72a8da0
Nathan903 <Nathan903@users.noreply.github.com>
2019-10-13 17:17:36 +0800

    Update server_bash.sh

--- Server/server_bash.sh
+++ Server/server_bash.sh
@@ -1,1 +1,9 @@
-server_bash.sh
+#run server_main.py on every startup of the server
+
+#how to write programs in bash
+#http://matt.might.net/articles/bash-by-example/
+
+#auto run bash in terminal
+#https://askubuntu.com/questions/335904/how-to-execute-bash-script-automatically-at-specific-interval
+
+echo "Hello world"
