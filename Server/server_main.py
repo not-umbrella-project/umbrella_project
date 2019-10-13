18bad6d995643e397ef6bdb50b242698c4a059ed
Nathan903 <Nathan903@users.noreply.github.com>
2019-10-13 17:00:03 +0800

    Create server_main.py

---
+++ Server/server_main.py
@@ -0,0 +1,22 @@
+#python3 server script
+import os
+from datetime import datetime
+
+
+def printfile(message):
+
+#update [client]_command.py to send command
+def command(message, client):
+
+#read [client]_data.py to get data
+def get_data(client):
+
+
+#main loop
+while True:
+	cycle_count +=1
+
+
+#error message
+error_message = "main loop terminated, with " + str(cycle_count) + " successful cycles"
+printfile(error_message)
