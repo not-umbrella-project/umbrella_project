6d31096882866870ef3f8a9a40944dc04901907d
Nathan903 <Nathan903@users.noreply.github.com>
2019-12-23 18:35:54 +0800

    update socket-test files

--- Server/test socket-client.py
+++ Server/test socket-client.py
@@ -2,14 +2,11 @@ import socket
 
 HOST = '192.168.1.15'
 PORT = 22222
-
+message = "aaa"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(b'f')
-data = s.recv(1024)
+s.sendall(message.encode())
+data = s.recv(4096)
 
 print('Received', repr(data))
-print(data)
-s.sendall(b'f')
-
 
--- Server/test socket-server.py
+++ Server/test socket-server.py
@@ -1,17 +1,20 @@
-import socket
 
+import socket
+print("debug")
 HOST = ''
 PORT = 22222
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    #https://docs.bitnami.com/virtual-machine/faq/administration/use-firewall/
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-            print(data)
+sends = b's'
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+
+print('Connected by', addr)
+
+data = conn.recv(4096)
+d= data.decode()
+print(type(d))
+if not data:
+    print("not data")
+
+conn.sendall(sends)
