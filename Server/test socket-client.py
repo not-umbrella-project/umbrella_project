702365afac6f1e2030d8edce997a9ab9048c9118
Nathan903 <Nathan903@users.noreply.github.com>
2020-04-12 11:59:28 +0800

    Update test socket-client.py

--- Server/test socket-client.py
+++ Server/test socket-client.py
@@ -1,7 +1,7 @@
 import socket
-#HOST = '34.80.90.238'
-HOST = '192.168.1.15'
-PORT = 22222
+HOST = '34.80.90.238'
+#HOST = '192.168.1.15'
+PORT = 22080
 message = "aaa"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
