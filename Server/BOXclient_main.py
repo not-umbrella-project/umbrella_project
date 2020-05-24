7bcb68d83b85480ed4be5d2fe3dfd20d4655d550
Nathan903 <Nathan903@users.noreply.github.com>
2020-05-24 23:17:56 +0800

    server_main intergration with socket

--- Server/BOXclient_main.py
+++ Server/BOXclient_main.py
@@ -1,5 +1,6 @@
 import time
 #send egress_message to server at (server_address, server_port), and return received message
+"""
 def talk_to_server(server_address, server_port, egress_message):
     #imports
     try:
@@ -9,13 +10,14 @@ def talk_to_server(server_address, server_port, egress_message):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_address, server_port))
-    s.sendall(egress_message.encode())
-    print("NEW socke")
+
+    #s.sendall(egress_message.encode())
+    print("NEW socket")
 
     i=1
-    while i<10:
+    while 1:#i<10:
+        print("here")
         s.sendall(str(i).encode())
-
         data = s.recv(4096)
         ingress_message = data.decode()
         #return ingress_message
@@ -25,10 +27,29 @@ def talk_to_server(server_address, server_port, egress_message):
         i+=1
     print("breaked")
     s.close()
+"""
+def talk_to_server(server_address, server_port, egress_message):
+    #imports
+    try:
+        import socket
+    except:
+        print("ERR cannot import: ", "socket")
 
-    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((server_address, server_port))
+    print("NEW socket created")
+    s.sendall(egress_message.encode())
+    print("sent "+egress_message+" to "+ server_address)
+    data = s.recv(4096)
+    ingress_message = data.decode()
+    #return ingress_message
+    return (ingress_message, type(ingress_message))
+    print("disconnect")
+    s.close()
 
-#print(talk_to_server('34.80.90.238', 22222,"aa"))
+
+print(talk_to_server('192.168.1.13', 33080,"aa"))
+"""
 print(talk_to_server('34.80.90.238', 33080,"aa\n"))
 print("also breaked")
 
@@ -37,3 +58,4 @@ print("sregr")
 print(talk_to_server('34.80.90.238', 33080,"aa\n"))
 
 
+"""
--- Server/working/server_main.py
+++ Server/working/server_main.py
@@ -4,41 +4,34 @@
 
 #imports
 try:
-        import logging
-        logging.basicConfig(filename="server_main_DOT_py.log", filemode="a", format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%Y-%M-%d %H:%M:%S", level=logging.DEBUG)
+    import logging
+    logging.basicConfig(filename="server_main_DOT_py.log", filemode="a", format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%Y-%M-%d %H:%M:%S", level=logging.INFO)
 except:
-        print("exception: cannot import logging")
+    print("exception: cannot import logging")
 try:
-	import os
-	import os.path
+    import os
+    import os.path
 except:
-	print("exception: cannot import os")
+    print("exception: cannot import os")
 try:
-	import subprocess
+    import subprocess
 except:
-	print("exception: cannot import subprocess")
+    print("exception: cannot import subprocess")
 try:
-	from datetime import datetime
-	import time
+    from datetime import datetime
+    import time
 except:
-	print("exception: cannot import datetime")
+    print("exception: cannot import datetime")
 try:
-	import socket
+    import socket
 except:
-	print("exception: cannot import socket")
+    print("exception: cannot import socket")
 logging.info("started")
 #https://stackoverflow.com/questions/3730964/python-script-execute-commands-in-terminal
 
 #body
+
 """
-def print_to_file(message):
-        #
-
-def log_line(mystr)
-"""
-
-
-#send data
 def p(HOST, PORT, message, client):
 
     #source: https://realpython.com/python-sockets/
@@ -56,88 +49,89 @@ def p(HOST, PORT, message, client):
                     break
                 conn.sendall(data)
                 print(data)
+                """
 
 
 def datawrite(filename, mystring):
-        fname = filename+".txt"
-        f = open(fname, "r+")
-        f.write(mystring)
-        f.close()
+    fname = filename+".txt"
+    f = open(fname, "r+")
+    f.write(mystring)
+    f.close()
 
 def dataappend(filename, mystring):
-        fname = filename+".txt"
-        f = open(fname, "a")
-        f.write("\n"+mystring)
-        f.close()
+    fname = filename+".txt"
+    f = open(fname, "a")
+    f.write("\n"+mystring)
+    f.close()
 
 def datanewfile(filename):
-        fname = filename+".txt"
-        f = open(fname, "x")
-        f.close()
+    fname = filename+".txt"
+    f = open(fname, "x")
+    f.close()
 
 def dataadduser(name):
-        #make sure no same name
-        try:
-                datanewfile(name)
-                datawrite(name, "r")
-                dataappend("1server_data_userList", name)
-        except:
-                logging.critical("trying to create new file with existing name")
+    #make sure no same name
+    try:
+        datanewfile(name)
+        datawrite(name, "r")
+        dataappend("1server_data_userList", name)
+    except:
+        logging.critical("trying to create new file with existing name")
 
 def isNewUser(name):
-        f = open("1server_data_userList.txt")
-        while True:
-                l = f.readline().strip("\n")
-                print(l)
-                if l:
-                        if l==(name):
-                                f.close()
-                                return True
-                else:
-                       break
-        f.close()
-        return False
+    f = open("1server_data_userList.txt")
+    while True:
+        l = f.readline().strip("\n")
+        if l:
+            if l==(name):
+                f.close()
+                return False
+        else:
+            break
+    f.close()
+    return True
 
 def datauserreturn(name):
-        line = "r@ "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        dataappend(name, line)
-        datawrite(name, "r")
+    line = "r@ "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    dataappend(name, line)
+    datawrite(name, "r")
+
+def datauserborrow(name):
+    line = "b@ "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    dataappend(name, line)
+    datawrite(name, "b")
 
 def dataReturned(name):
-        fname = name+".txt"
-        file = open(fname, "r")
-        h=file.readline(1)
-        if h=="r":
-                return True
-        elif h=="b":
-                return False
-        logging.critical("data file header isn't r or b")
-
+    fname = name+".txt"
+    file = open(fname, "r")
+    h=file.readline(1)
+    if h=="r":
+        return 1
+    elif h=="b":
+        return 0
+    logging.critical("data file header isn't r or b")
 
 def handleBorrow(name):
-        if isNewUser(name):
-                dataadduser(name)
-        if dataReturned(name):
-                #def datauserborrow(name)
-                line = "b@ "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                dataappend(name, line)
-                datawrite(name, "b")
-                ###OPEN
-        else:
-                return "don't open"
+    if isNewUser(name):
+        dataadduser(name)
+    if dataReturned(name)==1:
+        datauserborrow(name)
+        return "open, can borrow"
+    elif dataReturned(name) == 0:
+        return "don't open, because you haven't return yet "
+    else:
+        return "sorry there is a error"
+
+#def handleReturn(name):
 #integrity check
 
 #testing
-print(isNewUser("ddda"))
-dataadduser("ddda")
 
-if dataReturned("MEE"):
-        print(1111111)
-
-file = open("MEE.txt", "r")
+datauserreturn("ddda")
+handleBorrow("ddda")
+file = open("ddda.txt", "r")
 print("\n"+file.read())
 
-print(isNewUser("ddda"))
 
 
 #main loop
@@ -145,6 +139,38 @@ cycle_count = 0
 cycle_count_multiplier = 10
 i=1
 logging.info("main loop also started")
+
+HOST = ''
+PORT = 33080
+no_data = 0
+while 1:
+    #socket
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+    #logging.info
+    print('debug: Connected by', addr)
+    
+    while (conn.fileno() !=-1):
+        print('debug: 2nd while')
+        try:
+            data = conn.recv(4096)
+            print("data recieved:",data)
+            if not data:
+                not_data+=1
+                if not_data> 10:
+                    not_data=0
+                    print("not data")
+            else:
+                d= data.decode()
+            sent = handleBorrow(d)
+            print("sent data" + sent)
+            conn.sendall(sent.encode())
+        except:
+            print("Disconnected by",addr)
+            break
 """
 while True:
         UPDATE BOX STATUS
